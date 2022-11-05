beae3ef81153e6749f32e9b7fb7882cdf64c1f71
TharadonHeebnak <84135426+TharadonHeebnak@users.noreply.github.com>
2022-11-05 20:02:32 +0700

    หน้าสมัคร
    ชื่อ
    นามสกุล
    username
    เบอร์โทร
    รหัสผ่าน

---
+++ Admin_dashboard/register_form.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>ฟอร์มสมัคร</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div class="main-content">
+    <main-content><main>
+        <H1>กรอกข้อมูลลูกค้า</H1>
+    <form name="addata" action="savedata.php" method="post">
+            <table border="1" width="300">
+                <tr>
+                    <td>ชื่อ</td>
+                    <td><input type="text" name="textfirstname" size="30"></td>
+                </tr>
+                <tr>
+                    <td>นามสกุล</td>
+                    <td><input type="text" name="textlastname" size="30"></td>
+                </tr>
+                <tr>
+                    <td>username</td>
+                    <td><input type="text" name="textusername" size="30"></td>
+                </tr>
+                <tr>
+                    <td>เบอร์โทร</td>
+                    <td><input type="number" name="textphone" size="30"></td>
+                </tr>
+                <tr>
+                    <td>รหัสผ่าน</td>
+                    <td><input type="password" name="textpass" size="30"></td>
+                </tr>
+            </table>
+            <input type="submit" name="submit" value="ตกลง">
+        </form>
+    </main></main-content>
+    
+
+</div>
+</body>
+
+</html>
